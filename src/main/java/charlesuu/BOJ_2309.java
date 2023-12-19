f5293e0c459b2c67ff0d92d64383e50e2ff22855
charlesuu <tiger0192004@gmail.com>
2023-12-19 12:06:26 +0900

    [1주차] 브루트포스 BOJ_2309 이현호

--- src/main/java/charlesuu/BOJ_2309.java
+++ src/main/java/charlesuu/BOJ_2309.java
@@ -1,5 +1,36 @@
 package charlesuu;
 
-public class BOJ_2309 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
+public class BOJ_2309 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        int[] array = new int[9];
+        for (int i = 0; i < 9; i++) {
+            array[i] = sc.nextInt();
+            sum += array[i];
+        }
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - array[i] - array[j] == 100) {
+                    array[i] = 0;
+                    array[j] = 0;
+
+                    Arrays.sort(array);
+
+                    for (int k = 2; k < 9; k++) {
+                        System.out.println(array[k]);
+                    }
+                    return;
+                }
+            }
+        }
+    }
 }
