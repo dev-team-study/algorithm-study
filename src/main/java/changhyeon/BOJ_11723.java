b4e79334c03b52292ecf03889382b4aa6534257d
changhyeon <changhyeon.h@kakao.com>
2024-01-05 16:46:36 +0900

    [3주차] 브루트포스 BOJ_11723 황창현

--- src/main/java/changhyeon/BOJ_11723.java
+++ src/main/java/changhyeon/BOJ_11723.java
@@ -3,16 +3,12 @@ package changhyeon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class BOJ_11723 {
 
     private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    private static List<Integer> S = new ArrayList<>();
-    private static int M, MAX_SIZE = 2;
+    private static final StringBuilder stringBuilder = new StringBuilder();
+    private static int bit, M, MAX_SIZE = 2;
 
     public static void main(String[] args) throws IOException {
         M = Integer.parseInt(bufferedReader.readLine());
@@ -49,45 +45,37 @@ public class BOJ_11723 {
                     throw new RuntimeException("잘못된 입력입니다.");
             }
         }
+
+        System.out.println(stringBuilder.toString());
     }
 
     private static void empty() {
-        S.clear();
+        bit = 0;
     }
 
     private static void all() {
-        S = IntStream.range(1, 20)
-                .boxed()
-                .collect(Collectors.toList());
+        bit = (1 << 20) - 1;
     }
 
     private static void toggle(final int number) {
-        if (!S.contains(number)) {
-            S.add(number);
-            return;
-        }
+        bit = bit ^ (1 << number - 1);
 
-        S.remove(Integer.valueOf(number));
     }
 
     private static void check(final int number) {
-        if (!S.contains(number)) {
-            System.out.println(0);
+        if ((bit & (1 << (number - 1))) == 0) {
+            stringBuilder.append("0\n");
             return;
         }
 
-        System.out.println(1);
+        stringBuilder.append("1\n");
     }
 
     private static void remove(final int number) {
-        if (!S.contains(number)) {
-            S.remove(Integer.valueOf(number));
-        }
+        bit = bit & ~(1 << (number - 1));
     }
 
     private static void add(final int number) {
-        if (!S.contains(number)) {
-            S.add(number);
-        }
+        bit = bit | (1 << (number - 1));
     }
 }
