dce25636c2ef8290a9a9c9c85627b974eadc7d0e
changhyeon <changhyeon.h@kakao.com>
2024-01-09 11:51:36 +0900

    [4주차] DP BOJ_1463 황창현

---
+++ src/main/java/changhyeon/BOJ_1463.java
@@ -0,0 +1,33 @@
+package changhyeon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1463 {
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static int N, MAX_NUMBER = 1000000;
+    private static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(bufferedReader.readLine());
+        dp = new int[MAX_NUMBER];
+
+        dp[1] = 0;
+        dp[2] = 1;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
