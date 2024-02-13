0819f0cb4c25a6ee71e2f3ba6ef0770449f23218
changhyeon <changhyeon.h@kakao.com>
2024-02-13 14:58:21 +0900

    [9주차] 해시 PRO_의상 황창현

---
+++ src/main/java/changhyeon/PRO_의상.java
@@ -0,0 +1,24 @@
+package changhyeon;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public int solution(String[][] clothes) {
+        int sum = 1;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            String category = clothes[i][1];
+            map.put(category, map.getOrDefault(category, 0) + 1);
+        }
+
+        for (int num : map.values()) {
+            sum *= (num + 1);  // 안 입었을 때의 경우 1 추가
+        }
+
+        return sum - 1;
+    }
+}
