d29906cb5f9b8c0738df8481f9928951238cb7ac
BeomSeogKim <kbs4520@daum.net>
2024-02-14 13:41:29 +0900

    [9주차] 스택/큐 PRO_같은숫자는싫어 김범석

---
+++ src/main/java/tommy/PRO_같은숫자는싫어.java
@@ -0,0 +1,40 @@
+package tommy;
+
+import java.util.*;
+
+public class PRO_같은숫자는싫어 {
+    public int[] solution(String[] genres, int[] plays) {
+        List<Integer> answer = new ArrayList<>();
+
+        Map<String,Integer> gMap = new HashMap<>();
+        Map<String, Map<Integer, Integer>> pMap = new HashMap<>();  // index, play
+
+        for(int i = 0; i < genres.length; i++){
+            gMap.put(genres[i], gMap.getOrDefault(genres[i], 0) + plays[i]);
+            if(pMap.containsKey(genres[i])){
+                pMap.get(genres[i]).put(i, plays[i]);
+            } else{
+                Map<Integer, Integer> temp = new HashMap<>();
+                temp.put(i, plays[i]);
+                pMap.put(genres[i], temp);
+            }
+        }
+
+        List<String> keySet = new ArrayList<>(gMap.keySet());
+        Collections.sort(keySet, (a, b) -> gMap.get(b) - gMap.get(a));
+
+        for (String key : keySet){
+            Map<Integer, Integer> map = pMap.get(key);
+            List<Integer> genre_key = new ArrayList<>(map.keySet());
+
+            Collections.sort(genre_key, (a, b) -> map.get(b) - map.get(a));
+
+            answer.add(genre_key.get(0));
+            if (genre_key.size() > 1){
+                answer.add(genre_key.get(1));
+            }
+        }
+
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
